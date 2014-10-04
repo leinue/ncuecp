774426647386bf19cee1ca9571aee3347d905516
ivy <597055914@qq.com>
2014-10-04 21:34:29 +0800

    update class emailverification

--- fun/class.php
+++ fun/class.php
@@ -6,46 +6,51 @@ require('config.php');
 * abstract class EmailVerification
 * 用于注册/忘记密码/改变邮箱时的邮箱验证
 * 使用组合模式设计
+* **************************************************************************************
+* 注册时的消息示例
+* $to = $this->email;
+* $subject = "感谢注册NCUECP";
+* $regUrl="method=reg";
+* $message = "<a href=\"".$reg_url."\">欢迎注册NCUECP,请点击这里进行激活帐号</a>";
+* $from = "someonelse@example.com";
+* $headers = "From: $from";
 */
-abstract class EmailVerification{
+
+class EmailVerification{
 	protected $email;
 
 	function __construct($email){$this->email=$email;}
 
 	function getEmail(){return $this->email;}
 
-	abstract function send();
+	function send(emailContentMgr $ecm){mail($ecm->getTo(),$ecm->getSubject(),$ecm->getMessage(),$ecm->getHeaders());}
 }
 
 class emailContentMgr{
-	
-	function __construct(){
+	protected $subject;
+	protected $to;
+	protected $from;
+	protected $message;
+	protected $headers;
 
+	function __construct(
+		$subject,$to,$url,$from,$message,$headers){
+		$this->subject=$subject;
+		$this->to=$to;
+		$this->from=$from;
+		$this->message=$message;
+		$this->headers=$headers;
 	}
-}
 
-class regEmailVer extends EmailVerification{
-	function send(){
-		$to = $this->email;
-		$subject = "感谢注册NCUECP";
-		$reg_url="method=reg";
-		$message = "<a href=\"".$reg_url."\">欢迎注册NCUECP,请点击这里进行激活帐号</a>";
-		$from = "someonelse@example.com";
-		$headers = "From: $from";
-		mail($to,$subject,$message,$headers);
-	}
-}
+	function getSubject(){return $this->subject;}
 
-class forgotPasswordEmailVer extends EmailVerification{
-	function send(){
+	function getTo(){return $this->to;}
 
-	}
-}
+	function getFrom(){return $this->from;}
 
-class changeEmailVer extends EmailVerification{
-	function send(){
+	function getMessage(){return $this->message;}
 
-	}
+	function getHeaders(){return $this->headers;}
 }
 
 ?>
