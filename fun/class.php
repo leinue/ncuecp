3f8c33476ddda4b0c0c1fb1c329e70af2ec6c011
ivy <597055914@qq.com>
2014-10-05 17:29:00 +0800

    update class pdoOperation

--- fun/class.php
+++ fun/class.php
@@ -3,7 +3,66 @@
 require('config.php');
 
 /**
-* abstract class EmailVerification
+* class pdoOperation
+* 用于封装查询语句时的必须查询过程
+* @submitQuery()使用pdo的预处理语句进行查询,比较安全
+* @fetchClassQuery()返回对应数据库的class
+* @fetchOdd()返回单个值
+*
+*/
+class pdoOperation{
+
+	public $emailCheck="UPDATE `profile` SET `isValid`=`1` WHERE `uid`=?";
+
+	protected static $pdo;
+	
+	function __construct($pdo){$this->pdo=$pdo;}
+
+	function submitQuery($sql,$arr){
+		if(!(is_array($arr))){
+			return false;
+		}
+		$stmt=$this->pdo->prepare($sql);
+		$result=$stmt->execute($arr);
+		return $result;
+	}
+
+	function fetchClassQuery($sql,$arr,$className){
+		if(!(is_array($arr))){
+			return false;
+		}
+		$stmt=$this->pdo->prepare($sql);
+		$res=$stmt->execute($arr);
+
+		$stmt->setFetchMode(PDO::FETCH_CLASS,$className);
+
+		if ($res) {
+			if($draft=$stmt->fetchAll()) {
+				return $draft;
+			}else{
+				return false;}
+		}else{
+			return false;}
+	}
+
+	function fetchOdd($sql,$arr){
+		if(!(is_array($arr))){
+			return false;
+		}
+		$stmt=$this->pdo->prepare($sql);
+
+		if($stmt){
+			$stmt->execute($arr);
+			$row=$stmt->fetch();
+			if($row){
+				return $row[0];
+			}else{return false;}
+		}else{return false;}
+	}
+}
+
+/**
+* class EmailVerification
 * 用于注册/忘记密码/改变邮箱时的邮箱验证
 * 使用组合模式设计
 * **************************************************************************************
@@ -20,7 +79,12 @@ class EmailVerification{
 
 	function __construct(){}
 
-	function send(emailContentMgr $ecm){mail($ecm->getTo(),$ecm->getSubject(),$ecm->getMessage(),$ecm->getHeaders());}
+	function send(emailContentMgr $ecm){
+		mail($ecm->getTo(),$ecm->getSubject(),$ecm->getMessage(),$ecm->getHeaders());}
+
+	function verify(pdoOperation $pdoo,$uid){
+		return $pdoo->submitQuery($pdoo->emailCheck,array($uid));
+	}
 }
 
 class emailContentMgr{
@@ -54,6 +118,5 @@ $em=new emailContentMgr("感谢注册NCUECP","url","597055914@qq.com","message",
 $ev=new EmailVerification();
 $ev->send($em);
 */
-
 
 ?>
